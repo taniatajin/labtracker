75135e11554bd2218ef53b77e53e56b394c785f1
taniatajin <tania15-4692@diu.edu.bd>
2025-04-11 20:50:08 +0600

    prb11

---
+++ lib/prb11.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BottomNavApp());
+}
+
+class BottomNavApp extends StatelessWidget {
+  const BottomNavApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Bottom Navigation App',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: const MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = const [
+    HomeTab(),
+    SearchTab(),
+    ProfileTab(),
+  ];
+
+  void _onNavTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AnimatedSwitcher(
+        duration: const Duration(milliseconds: 300),
+        child: _screens[_currentIndex],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        selectedItemColor: Colors.teal,
+        unselectedItemColor: Colors.grey,
+        onTap: _onNavTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class HomeTab extends StatelessWidget {
+  const HomeTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Home Tab',
+            style: TextStyle(fontSize: 24),
+          ),
+          const SizedBox(height: 20),
+          Image.asset(
+            'assets/images/sample.avif', // Fixed typo from 'assetes' to 'assets'
+            width: 150,
+            height: 150,
+            fit: BoxFit.cover,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SearchTab extends StatelessWidget {
+  const SearchTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Search Tab',
+            style: TextStyle(fontSize: 24),
+          ),
+          SizedBox(height: 20),
+          Icon(
+            Icons.search,
+            size: 100,
+            color: Colors.blue,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ProfileTab extends StatelessWidget {
+  const ProfileTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Profile Tab',
+            style: TextStyle(fontSize: 24),
+          ),
+          SizedBox(height: 20),
+          Icon(
+            Icons.person,
+            size: 100,
+            color: Colors.green,
+          ),
+        ],
+      ),
+    );
+  }
+}
